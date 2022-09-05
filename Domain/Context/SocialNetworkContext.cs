2e243bd992da6e162c3471999ea8203e6177792b
cyberdragon42 <panchyshynmariait@gmail.com>
2022-09-05 14:02:32 +0300

    added database

---
+++ Domain/Context/SocialNetworkContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Context
+{
+    public class SocialNetworkContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public SocialNetworkContext(DbContextOptions<SocialNetworkContext> options)
+        : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(x =>
+            {
+                x.Property(y => y.Id).HasDefaultValueSql("NEWID()");
+            });
+        }
+    }
+}
---
+++ Domain/Migrations/20220904230545_Initial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Domain.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Userss",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false, defaultValueSql: "NEWID()"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Userss", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Userss");
+        }
+    }
+}
